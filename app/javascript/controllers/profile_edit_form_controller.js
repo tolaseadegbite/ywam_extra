613a573f278e99476f82d40c8a8574a18aadf78b
tolaseadegbite <tolasekelvin@gmail.com>
2024-09-04 20:53:25 +0100

    Added profile cover image and avatar preview feature

--- app/javascript/controllers/profile_edit_form_controller.js
+++ app/javascript/controllers/profile_edit_form_controller.js
@@ -2,18 +2,40 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="profile-edit-form"
 export default class extends Controller {
-  static targets = ['selectCoverImageButton', 'coverImageFile', 'selectAvatarButton', 'avatarFile']
+  static targets = ['selectCoverImageButton', 'coverImageFile', 'coverImagePreview', 'selectAvatarButton', 'avatarFile', 'avatarPreview']
 
   connect() {
+    // Initialize any state or setup event listeners here
   }
 
-  selectCoverImage(e) {
-    e.preventDefault();
+  selectCoverImage(event) {
+    event.preventDefault()
     this.coverImageFileTarget.click()
   }
 
-  selectAvatar(e) {
-    e.preventDefault();
+  selectAvatar(event) {
+    event.preventDefault()
     this.avatarFileTarget.click()
+  }
+
+  previewImage(fileInput, previewTarget) {
+    if (!fileInput.files.length || !fileInput.files[0]) return
+
+    const file = fileInput.files[0]
+    const reader = new FileReader()
+
+    reader.onload = (event) => {
+      previewTarget.src = event.target.result
+    }
+
+    reader.readAsDataURL(file)
+  }
+
+  coverImageChanged() {
+    this.previewImage(this.coverImageFileTarget, this.coverImagePreviewTarget)
+  }
+
+  avatarChanged() {
+    this.previewImage(this.avatarFileTarget, this.avatarPreviewTarget)
   }
 }
