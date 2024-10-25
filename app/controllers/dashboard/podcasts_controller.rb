940e3eccc43a2da2b2318dddc0a5eb6c8f6e1b8d
tolaseadegbite <tolasekelvin@gmail.com>
2024-10-25 09:48:02 +0100

    Created authorization layer for dashboard show pages

--- app/controllers/dashboard/podcasts_controller.rb
+++ app/controllers/dashboard/podcasts_controller.rb
@@ -1,6 +1,7 @@
 class Dashboard::PodcastsController < ApplicationController
   before_action :authenticate_account!
   before_action :find_podcast, only: %w[show edit update destroy]
+  before_action :restrict_user, only: %[show]
 
   def index
     @podcasts = current_account.podcasts.includes(:account).ordered
@@ -11,7 +12,16 @@ class Dashboard::PodcastsController < ApplicationController
   end
 
   def create
+    @podcast = current_account.podcasts.build(podcast_params)
 
+    if @podcast.save
+      respond_to do |format|
+        format.html { redirect_to dashboard_podcast_url(@podcast), notice: "Podcast created successfully" }
+        # format.turbo_stream { flash.now[:notice] = 'Podcast created successfully' }
+      end
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def show
@@ -45,5 +55,11 @@ class Dashboard::PodcastsController < ApplicationController
 
     def find_podcast
       @podcast = Podcast.find(params[:id])
+    end
+
+    def restrict_user
+      unless current_account == @podcast.account
+        redirect_to dashboard_podcast_url, notice: 'Access denied!'
+      end
     end
 end
